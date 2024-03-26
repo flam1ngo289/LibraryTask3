04fc5d56709ed98e36deb0168ccf83fc30827a45
flam1ngo <kris.s.03@mail.ru>
2024-03-26 22:07:14 +0300

    Initial commit

---
+++ DynamicLeaver.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <string>
+#ifdef LEAVERLIBRARYDYNAMIC_EXPORTS
+#define LEAVERLIBRARY_API __declspec(dllexport)
+#else
+#define LEAVERLIBRARY_API __declspec(dllimport)
+#endif
+
+class Leaver {
+public:
+	std::string leave(std::string& input);
+};
